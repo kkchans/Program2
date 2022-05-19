1a13da7b66b53d9775e2d20a6d4cbda2e51699f8
kim chanhee <s2008@e-mirim.hs.kr>
2022-05-19 11:29:26 +0900

    초기 커밋.

---
+++ Program2/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program2
+{
+    internal class Car
+    {
+        int carNumber;
+        DateTime inTime;
+        DateTime outTime;
+
+        public void SetInTime()
+        {
+            this.inTime = DateTime.Now;
+
+        }
+        public void SetOutTime()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.SetInTime();
+            car.SetOutTime();
+
+            Random random = new Random();
+            random.Next();
+        }
+    }
+}
